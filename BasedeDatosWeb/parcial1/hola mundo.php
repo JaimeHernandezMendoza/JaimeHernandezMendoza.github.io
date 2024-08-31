03517e4d7612451854e0f1094adbe2ffd9db6728
Jaime Hernández Mendoza <159406670+JaimeHernandezMendoza@users.noreply.github.com>
2024-08-30 18:54:48 -0700

    Add files via upload

---
+++ BasedeDatosWeb/parcial1/hola mundo.php
@@ -0,0 +1,20 @@
+<?php
+
+  $nombre = "Alonso";
+  $numero = 4;
+  $numero2 = "4";
+
+  $total = $numero + $numero2;
+
+  echo "Total = ", $total;
+
+  echo "<h1> HOLA MUNDO </h1>";
+
+  for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+
+  }
+
+  echo "<h3 style='color: red;'>Hernandez Mendoza Jaime Fernando</h3>";
+
+?>
