22d4ea09d9dc08d91f184bc98e5a5a95d2220f29
Jaime Hernández Mendoza <159406670+JaimeHernandezMendoza@users.noreply.github.com>
2024-10-16 18:42:41 -0700

    Add files via upload

--- BasedeDatosWeb/parcial2/practica9/practica9.js
+++ BasedeDatosWeb/parcial2/practica9/practica9.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     let cartas = new Array(54).fill(false);
-
+    var cont = 0;
     $("#dar_carta").click(function(){
         var b = true;
         var cont = 0;
@@ -21,4 +21,39 @@ $(document).ready(function(){
                  }
         }
     });
+
+    cargaCartas();
+    function cargaCartas(){
+        var carta1 = 0;
+        var carta2 = 0;
+        let asignadas = new Array(54).fill(false);
+        while(carta1 < 16){
+            var num = Math.floor(Math.random()*54) + 1;
+            if(asignadas[num-1] == false){
+                $("#carta1").append("<div class ='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true;
+                carta1++;
+            } else{
+                continue;
+            }
+        }
+    }
+
+    cargaCartas();
+    function cargaCartas(){
+        var carta1 = 0;
+        var carta2 = 0;
+        let asignadas = new Array(54).fill(false);
+        while(carta2 < 16){
+            var num = Math.floor(Math.random()*54) + 1;
+            if(asignadas[num-1] == false){
+                $("#carta1").append("<div class ='col-sm-3'><img class='img-fluid' src='loteria/"+num+".jpg'></div>");
+                asignadas[num-1] = true;
+                carta2++;
+            } else{
+                continue;
+            }
+        }
+    }
+
 });
